b3183e29dd690ef6e58d0b5539a8e98227eb506c
John Dickson <johnslash8@gmail.com>
2017-10-14 17:56:22 -0700

    fixed how data is presented

--- web/project1/bridgeStats.php
+++ web/project1/bridgeStats.php
@@ -14,22 +14,22 @@
 	<body>
 
 		<form action="getBridgeStats.php" method="post">
-			<input type="text" name="user">
+			<input type="text" name="player">
 			<input type="text" name="game">
 			<input type="text" name="round">
 			<input type="submit" value="Get Stats" name="submit">
 		</form>
 
 		<?php
-			$statement = $db->prepare("SELECT * FROM user");
+			$statement = $db->prepare("SELECT * FROM player");
 			$statement->execute();
 
 			// Go through each result
 			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 			{
 				echo '<p>';
-				echo '<strong>' . $row['username'] . ' ' . $row['password'] . ':';
-				echo $row['display_name'] . '</strong>' . ' - ' . $row['average_bid'] . ' - ' . $row['bid_sucess_rate'];
+				echo $row['username'] . ' - ' . $row['password'] . ' - ';
+				echo $row['display_name'] . ' - ' . $row['average_bid'] . ' - ' . $row['bid_sucess_rate'];
 				echo '</p>';
 			}
 
@@ -40,8 +40,8 @@
 			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 			{
 				echo '<p>';
-				echo '<strong>' . $row['player1_id'] . ' ' . $row['player2_id'] . ':';
-				echo $row['player3_id'] . '</strong>' . ' - ' . $row['player4_id'] . ' - ' . $row['rounds'] . ' - ' . $row['score'] . ' - ' . $row['win'] . ' - ' . $row['average_bid'] . ' - ' . $row['bid_sucess_rate'];
+				echo $row['player1_id'] . ' - ' . $row['player2_id'] . ' - ';
+				echo $row['player3_id'] . ' - ' . $row['player4_id'] . ' - ' . $row['rounds'] . ' - ' . $row['score'] . ' - ' . $row['win'] . ' - ' . $row['average_bid'] . ' - ' . $row['bid_sucess_rate'];
 				echo '</p>';
 			}
 
@@ -52,8 +52,8 @@
 			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 			{
 				echo '<p>';
-				echo '<strong>' . $row['game_id'] . ' ' . $row['round_number'] . ':';
-				echo $row['player1_bid'] . '</strong>' . ' - ' . $row['player2_bid'] . ' - ' . $row['player3_bid'] . ' - ' . $row['player4_bid'] . ' - ' . $row['team1_tricks'] . ' - ' . $row['team2_tricks'] . ' - ' . $row['team1_gotbid'] . ' - ' . $row['team2_gotbid'] . ' - ' . $row['team1_points'] . ' - ' . $row['team2_points'];
+				echo $row['game_id'] . ' - ' . $row['round_number'] . ' - ';
+				echo $row['player1_bid'] . ' - ' . $row['player2_bid'] . ' - ' . $row['player3_bid'] . ' - ' . $row['player4_bid'] . ' - ' . $row['team1_tricks'] . ' - ' . $row['team2_tricks'] . ' - ' . $row['team1_gotbid'] . ' - ' . $row['team2_gotbid'] . ' - ' . $row['team1_points'] . ' - ' . $row['team2_points'];
 				echo '</p>';
 			}
 		?>
--- web/project1/getBridgeStats.php
+++ web/project1/getBridgeStats.php
@@ -9,15 +9,15 @@
 	<body>
 
 	<?php
-			$statement = $db->prepare("SELECT * FROM user WHERE user = $_POST['user']");
+			$statement = $db->prepare("SELECT * FROM player WHERE player = $_POST['player']");
 			$statement->execute();
 
 			// Go through each result
 			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 			{
 				echo '<p>';
-				echo '<strong>' . $row['username'] . ' ' . $row['password'] . ':';
-				echo $row['display_name'] . '</strong>' . ' - ' . $row['average_bid'] . ' - ' . $row['bid_sucess_rate'];
+				echo $row['username'] . ' - ' . $row['password'] . ' - ';
+				echo $row['display_name'] . ' - ' . $row['average_bid'] . ' - ' . $row['bid_sucess_rate'];
 				echo '</p>';
 			}
 
@@ -28,8 +28,8 @@
 			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 			{
 				echo '<p>';
-				echo '<strong>' . $row['player1_id'] . ' ' . $row['player2_id'] . ':';
-				echo $row['player3_id'] . '</strong>' . ' - ' . $row['player4_id'] . ' - ' . $row['rounds'] . ' - ' . $row['score'] . ' - ' . $row['win'] . ' - ' . $row['average_bid'] . ' - ' . $row['bid_sucess_rate'];
+				echo $row['player1_id'] . ' - ' . $row['player2_id'] . ' - ';
+				echo $row['player3_id'] . ' - ' . $row['player4_id'] . ' - ' . $row['rounds'] . ' - ' . $row['score'] . ' - ' . $row['win'] . ' - ' . $row['average_bid'] . ' - ' . $row['bid_sucess_rate'];
 				echo '</p>';
 			}
 
@@ -40,8 +40,8 @@
 			while ($row = $statement->fetch(PDO::FETCH_ASSOC))
 			{
 				echo '<p>';
-				echo '<strong>' . $row['game_id'] . ' ' . $row['round_number'] . ':';
-				echo $row['player1_bid'] . '</strong>' . ' - ' . $row['player2_bid'] . ' - ' . $row['player3_bid'] . ' - ' . $row['player4_bid'] . ' - ' . $row['team1_tricks'] . ' - ' . $row['team2_tricks'] . ' - ' . $row['team1_gotbid'] . ' - ' . $row['team2_gotbid'] . ' - ' . $row['team1_points'] . ' - ' . $row['team2_points'];
+				echo $row['game_id'] . ' - ' . $row['round_number'] . ' - ';
+				echo $row['player1_bid'] . ' - ' . $row['player2_bid'] . ' - ' . $row['player3_bid'] . ' - ' . $row['player4_bid'] . ' - ' . $row['team1_tricks'] . ' - ' . $row['team2_tricks'] . ' - ' . $row['team1_gotbid'] . ' - ' . $row['team2_gotbid'] . ' - ' . $row['team1_points'] . ' - ' . $row['team2_points'];
 				echo '</p>';
 			}
 		?>
