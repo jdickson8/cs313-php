29e0bc405edeaf8d741466f509605db4c459d6b7
John Dickson <johnslash8@gmail.com>
2017-11-08 16:04:58 -0800

    adding heroku

--- web/week09/express_example/app.js
+++ web/week09/express_example/app.js
@@ -14,6 +14,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
