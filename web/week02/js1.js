88365f5ba814a710d3308cb6d7fc86dbeac67705
John Dickson <johnslash8@gmail.com>
2018-09-26 17:40:19 -0600

    Working on team assignment week 02

--- web/week02/js1.js
+++ web/week02/js1.js
@@ -2,14 +2,26 @@ function clickMe() {
 	alert("Clicked!");
 }
 
-function changeColor() {
-	var textbox_id = "txtColor";
+function changeDiv1Color() {
+	var textbox_id = "txt1Color";
 	var textbox = document.getElementById(textbox_id);
 
 	var div_id = "div1";
 	var div = document.getElementById(div_id);
 
-	var color = textbox.value;
+	var color = "#" + textbox.value;
+	div.style.backgroundColor = color;
+
+}
+
+function changeDiv2Color() {
+	var textbox_id = "txt2Color";
+	var textbox = document.getElementById(textbox_id);
+
+	var div_id = "div2";
+	var div = document.getElementById(div_id);
+
+	var color = "#" + textbox.value;
 	div.style.backgroundColor = color;
 
 }
