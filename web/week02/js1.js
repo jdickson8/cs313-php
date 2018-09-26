515ff6ba18abf4c49541fb0e2152ba19b1de3b77
John Dickson <johnslash8@gmail.com>
2018-09-26 17:55:01 -0600

    Working on team assignment week 02

--- web/week02/js1.js
+++ web/week02/js1.js
@@ -18,10 +18,17 @@ function changeDiv2Color() {
 	var textbox_id = "txt2Color";
 	var textbox = document.getElementById(textbox_id);
 
-	var div_id = "div2";
-	var div = document.getElementById(div_id);
-
 	var color = "#" + textbox.value;
 	div.style.backgroundColor = color;
 
+	$("div2").on("click", "button", function(event) {
+		$(event.delegateTarget).css("background-color", color);
+	})
+
+}
+
+function hideDiv3() {
+	$("div3").on("click", "button", function(event) {
+		$(event.delegateTarget).fadeToggle("slow", "linear");
+	})
 }
