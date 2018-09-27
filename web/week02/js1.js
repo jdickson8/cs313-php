f5adc09464488d01a2f09b2f9aaa7e60704fb897
John Dickson <johnslash8@gmail.com>
2018-09-26 18:51:25 -0600

    Working on team assignment week 02

--- web/week02/js1.js
+++ web/week02/js1.js
@@ -1,5 +1,6 @@
 $(document).ready(function() { 
 	$("#div2button").one('click', function() {
+		var color = "#" + document.getElementById("txt2Color").value;
 		$("#div2").css("background-color", color);
 	})
 
@@ -13,21 +14,9 @@ function clickMe() {
 }
 
 function changeDiv1Color() {
-	var textbox_id = "txt1Color";
-	var textbox = document.getElementById(textbox_id);
+	var color = "#" + document.getElementById("txt1Color").value;
+	var div = document.getElementById("div1");
 
-	var div_id = "div1";
-	var div = document.getElementById(div_id);
-
-	var color = "#" + textbox.value;
 	div.style.backgroundColor = color;
-
-}
-
-function changeDiv2Color() {
-	var textbox_id = "txt2Color";
-	var textbox = document.getElementById(textbox_id);
-
-	var color = "#" + textbox.value;
 }
 
